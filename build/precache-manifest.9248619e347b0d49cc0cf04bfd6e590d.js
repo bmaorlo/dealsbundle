0c42925c7955e95c0a88984782fe2882fcfe510c
Bohdan Didyk <bogdand@elyotech.com>
2020-03-03 16:52:33 +0200

    build

---
+++ build/precache-manifest.9248619e347b0d49cc0cf04bfd6e590d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693d7db86b4c825b4a136499f5e5a624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a1390c66ae5773c594a",
+    "url": "/static/css/2.661b9127.chunk.css"
+  },
+  {
+    "revision": "df34d263723b86572135",
+    "url": "/static/css/main.cc9cf60c.chunk.css"
+  },
+  {
+    "revision": "5a1390c66ae5773c594a",
+    "url": "/static/js/2.18b1dc88.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.18b1dc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df34d263723b86572135",
+    "url": "/static/js/main.b6b0733c.chunk.js"
+  },
+  {
+    "revision": "ff40b17a50eefde4af20",
+    "url": "/static/js/runtime-main.f3c451fe.js"
+  }
+]);
